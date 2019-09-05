03771795a0ccf00548b6e7b3bded5cc569f8e7a9
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-09-05 18:34:37 -0500

    updates to html files and server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,7 @@
 //1. A GET Route to /survey which should display the survey page.
 //2. A default, catch-all route that leads to home.html which displays the home page.
+
+// Routes
+app.get("/", function (req, res) {
+    res.send("../public/home.html");
+});
