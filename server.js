b7dfd9285aa9818a2afcdfffa8b37b33e19b2614
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-09-05 20:40:13 -0500

    removed handlebars

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 var express = require("express");
 var path = require("path");
 var mysql = require("mysql");
-var exphbs = require("express-handlebars");
 
 var app = express();
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public")); // static files go in public folder
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var connection = mysql.createConnection({
     host: "localhost",
